cf73a1c970f4da6778de504eb6344dcec4ea076c
CustomHits <103044856+CustomHits@users.noreply.github.com>
2022-12-11 19:05:10 +0000

    email progress

--- email.php
+++ email.php
@@ -3,12 +3,47 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
-    require("requirements/dbInformation.php")
+    require("requirements/dbInformation.php");
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
 
 
+
+
+
+function all_watch_lists($firstName, $email, $itemName){
+    $mail = new PHPMailer(true);
+
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'db.20223.mev@gmail.com';
+    $mail -> Password = 'zaxwkqmjovaoscss';
+    $mail -> SMTPSecure = 'ssl';
+    $mail -> Port = 465;
+
+    $mail -> setFrom('db.20223.mev@gmail.com', 'MHM Goods');
+    
+    $mail-> addAddress($email, $firstName);
+
+    $mail -> isHTML(true);
+
+    $mail -> Subject = 'Auction update: new bid on '.$itemName;
+    $mail -> Body = "Hello ".$firstName.", <br><br> There's a new high bid on <a href='http://localhost/auction/listing.php?auction_id=".$itemName."'> auction</a> you are watching.<br><br>
+    If you are still interested in this item, please log in to place a new bid. 
+    <br><br>
+    You are receiving this e-mail because ".$itemName." is in your watchlist, to stop recieving email notifications 
+    please remove the item from your watchlist.
+    <br><br>
+    Kind regards,
+    <br><br>
+    MHM Goods";
+    $mail -> send();
+
+}
+
+function buyer_email($itemName, $email, $firstName){
 
     $mail = new PHPMailer(true);
 
@@ -20,32 +55,8 @@
     $mail -> SMTPSecure = 'ssl';
     $mail -> Port = 465;
 
-    $mail->setFrom('');
     $mail -> setFrom('db.20223.mev@gmail.com', 'MHM Goods');
 
-function all_watch_lists($firstName, $email, $itemName){
-    global $mail;
-    
-    $mail-> addAddress($email, $firstName);
-
-    $mail -> isHTML(true);
-    
-    $mail -> Subject = 'Auction update: new bid on '.$itemName;
-    $mail -> Body = "Hello ".$firstName.", <br><br> There's a new high bid item ".$itemName."<br><br>
-    If you are still interested in this item, please log in to place a new bid. <br><br> 
-    You are receiving this e-mail because ".$itemName." is in your watchlist, to stop recieving email notifications 
-    please remove the item from your watchlist.
-    
-    Kind regards,
-    
-    MHM Goods"
-    $mail -> send();
-
-}
-
-function buyer_email($itemName, $email, $firstName){
-
-    global $mail;
     $mail->addAddress($email, $firstName);
     $mail->isHTML(true);
     $mail->Subject = "You won the ".$itemName;
@@ -54,17 +65,51 @@ function buyer_email($itemName, $email, $firstName){
     
     Kind regards,
     
-    MHM Goods"
+    MHM Goods";
     
     $mail->send();
 }
 
-function seller_email($itemName, $email, $firstName, $email) {
-    global $mail
+function seller_email($itemName, $email, $firstName) {
+    $mail = new PHPMailer(true);
+
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'db.20223.mev@gmail.com';
+    $mail -> Password = 'zaxwkqmjovaoscss';
+    $mail -> SMTPSecure = 'ssl';
+    $mail -> Port = 465;
+
+    $mail -> setFrom('db.20223.mev@gmail.com', 'MHM Goods');
+
     $mail -> addAddress($email, $firstName);
 
     $mail -> isHTML(true);
-    $mail -> Subject = $itemName." has sold!"
+    $mail -> Subject = $itemName." has sold!";
+    $mail -> Body = "item has sold";
+
+    $mail->send();
+}
+
+function no_sale($email, $firstName){
+    $mail = new PHPMailer(true);
+
+    $mail -> isSMTP();
+    $mail -> Host = 'smtp.gmail.com';
+    $mail -> SMTPAuth = true;
+    $mail -> Username = 'db.20223.mev@gmail.com';
+    $mail -> Password = 'zaxwkqmjovaoscss';
+    $mail -> SMTPSecure = 'ssl';
+    $mail -> Port = 465;
+
+    $mail -> setFrom('db.20223.mev@gmail.com', 'MHM Goods');
+
+    $mail -> addAddress($email, $firstName);
+    $mail -> isHTML(true);
+    $mail -> Body = 'item did not sell';
+
+    $mail->send();
 }
 
 ?>
--- place_bid.php
+++ place_bid.php
@@ -1,4 +1,5 @@
-<?php require("requirements/dbInformation.php")?> 
+<?php require("requirements/dbInformation.php") ?>
+<?php require("email.php")?> 
 
 <?php
 session_start();
@@ -38,7 +39,34 @@ if ($current_price < $bid_price){
     $updateQuery = "UPDATE `auctions` SET `starting_price`={$bid_price} WHERE auction_id = {$item_id}";
     $resultObj = $connection->query($submit_bid);
     $resultObj = $connection->query($updateQuery);
+
+    // query the email of all the users watching
+
+    $watchlist_users = "SELECT users.email, users.first_name, users.user_id FROM users INNER JOIN watch ON watch.user_id=users.user_id WHERE watch.auction_id = {$item_id}";
+    $usersObj = $connection->query($watchlist_users)->fetch_all();
+    
+    foreach ($usersObj as $row) {
+            $email = $row[0];
+            $firstName = $row[1];
+            $userID = (int) $row[2];
+
+            if ($userID != $buyer_id){
+                all_watch_lists($firstName, $email, $item_id);
+            }
+        }
+        
+    // add to the watchlist (if he isn't watching it already)
+    $watching = FALSE;
+    $check_watching = "SELECT * FROM `watch` WHERE `user_id` = {$buyer_id} AND `auction_id` = {$item_id}";
+    $watchObj = $connection->query($check_watching)->fetch_assoc();
+        
+    if (!$watchObj){
+        $query = "INSERT INTO `watch` (`watch_id`, `user_id`, `auction_id`) VALUES (NULL, {$buyer_id}, {$item_id})";
+        $resultObj = $connection->query($query);
+    }
+
     echo "Create bid successfully! <a href='mybids.php'>View my bids</a>.";
+
 } else {
     echo('<div class="text-center">Invalid bid. Please input a value greater than the current price.<br>You\'re being redirected to auction page.</div>');
     header("refresh:3;'listing.php?auction_id=".$item_id);
